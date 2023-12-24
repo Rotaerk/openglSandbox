50cbcd0d5466a4c1e97a7e63c7e3ecbd012040a0
rotaerk <m.scott.stewart@gmail.com>
2023-12-23 19:27:21 -0500

    I see a triangle.

--- OpenGLSandbox/src/OpenGLSandbox.hs
+++ OpenGLSandbox/src/OpenGLSandbox.hs
@@ -4,18 +4,26 @@ module Main where
 
 import Prelude.Local
 
---import Paths_OpenGLSandbox
+import Paths_OpenGLSandbox
 
+import ApplicationException
 import Control.Exception
 import Control.Monad.Extra
 import Control.Monad.IO.Class
 import Control.Monad.Trans.Resource
 import Data.Acquire.Local
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Internal as BS
+import qualified Data.ByteString.Unsafe as BS
 import Data.Function
 import Data.Functor
 import Data.IORef
-import qualified Data.Vector.Storable as V
+import Data.Primitive.PrimArray
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
 import Foreign.Marshal.Alloc
+import Foreign.Marshal.Utils as FMU
+import Foreign.Ptr
 import Foreign.Storable
 import Graphics.GL.Core33
 import qualified Graphics.UI.GLFW.Local as GLFW
@@ -24,6 +32,10 @@ import System.Clock
 main :: IO ()
 main = do
   runResourceT resourceMain
+  `catch` (
+    \(e :: ApplicationException) ->
+      putStrLn $ displayException e
+  )
   `catch` (
     \(e :: GLFW.GLFWException) ->
       putStrLn $ displayException e
@@ -56,17 +68,54 @@ resourceMain = do
     pure ref
   ioPutStrLn "Window framebuffer size callback registered."
 
-  vboId <- liftIO $ alloca $ \vboIdPtr -> do
-    glGenBuffers 1 vboIdPtr
-    peek vboIdPtr
-  ioPutStrLn $ "Created VBO with ID " ++ show vboId
+  shaderProgramId <- liftIO $ do
+    vertShaderId <- compileShader "triangle.vert" GL_VERTEX_SHADER
+    fragShaderId <- compileShader "triangle.frag" GL_FRAGMENT_SHADER
+    shaderProgramId <- glCreateProgram
+    glAttachShader shaderProgramId vertShaderId
+    glAttachShader shaderProgramId fragShaderId
+    glLinkProgram shaderProgramId
+    linkingFailed <- alloca $ \linkStatusPtr -> do
+      glGetProgramiv shaderProgramId GL_LINK_STATUS linkStatusPtr
+      (GL_FALSE ==) . fromIntegral <$> peek linkStatusPtr
+    when linkingFailed $ do
+      let maxLen = 512
+      infoLogText <- fmap T.decodeUtf8Lenient . BS.createUptoN maxLen $ \infoLogBsPtr ->
+        alloca $ \actualLenPtr -> do
+          glGetProgramInfoLog shaderProgramId (fromIntegral maxLen) actualLenPtr (castPtr infoLogBsPtr)
+          fromIntegral <$> peek actualLenPtr
+      throwAppExM $ "Shader program link error: " ++ T.unpack infoLogText
+    glUseProgram shaderProgramId
+    glDeleteShader vertShaderId
+    glDeleteShader fragShaderId
+    return shaderProgramId
+  ioPutStrLn "Shader program created."
 
-  liftIO $ glBindBuffer GL_ARRAY_BUFFER vboId
-  ioPutStrLn "Bound new VBO to GL_ARRAY_BUFFER."
+  vertexArrayId <- liftIO $ do
+    (vertexArrayId, vertexArrayBufferId) <- alloca $ \idPtr ->
+      (,) <$>
+      (glGenVertexArrays 1 idPtr >> peek idPtr) <*>
+      (glGenBuffers 1 idPtr >> peek idPtr)
 
-  liftIO $ V.unsafeWith triangleVertices $ \triangleVerticesPtr -> do
-    glBufferData GL_ARRAY_BUFFER (fromIntegral $ V.length triangleVertices * sizeOf (undefined :: Float)) triangleVerticesPtr GL_STATIC_DRAW
-  ioPutStrLn "Filled VBO."
+    glBindVertexArray vertexArrayId
+    glBindBuffer GL_ARRAY_BUFFER vertexArrayBufferId
+
+    let numVertices = length triangleVertices
+    verticesArray <- newAlignedPinnedPrimArray numVertices
+    forM_ (zip [0..] triangleVertices) $ \(idx, vertex) ->
+      writePrimArray verticesArray idx vertex
+    void $ withMutablePrimArrayContents verticesArray $ \verticesArrayPtr -> do
+      glBufferData GL_ARRAY_BUFFER (fromIntegral $ numVertices * sizeOf (undefined :: Float)) verticesArrayPtr GL_STATIC_DRAW
+      return undefined -- works around bug in type signature
+
+    glVertexAttribPointer 0 3 GL_FLOAT GL_FALSE (fromIntegral $ 3 * sizeOf (undefined :: Float)) nullPtr
+    glEnableVertexAttribArray 0
+
+    glBindBuffer GL_ARRAY_BUFFER 0
+    glBindVertexArray 0
+
+    return vertexArrayId
+  ioPutStrLn "Vertex array created."
 
   ioPutStrLn "Render loop starting."
   liftIO $ fix $ \renderLoop ->
@@ -78,21 +127,40 @@ resourceMain = do
         renderLoop
       GLFW.WindowClosed -> pure ()
       GLFW.WindowReady -> do
-        processFrame window
+        whenM (GLFW.getKey window GLFW.Key'Escape <&> (== GLFW.KeyState'Pressed)) $
+          GLFW.setWindowShouldClose window True
+        glClearColor 0.2 0.3 0.3 1
+        glClear GL_COLOR_BUFFER_BIT
+        glUseProgram shaderProgramId
+        glBindVertexArray vertexArrayId
+        glDrawArrays GL_TRIANGLES 0 3
         GLFW.swapBuffers window
         renderLoop
 
-processFrame :: GLFW.Window -> IO ()
-processFrame window = do
-  whenM (GLFW.getKey window GLFW.Key'Escape <&> (== GLFW.KeyState'Pressed)) $
-    GLFW.setWindowShouldClose window True
-  glClearColor 0.2 0.3 0.3 1
-  glClear GL_COLOR_BUFFER_BIT
-
-triangleVertices :: V.Vector Float
-triangleVertices = V.fromList
+triangleVertices :: [Float]
+triangleVertices =
   [
     -0.5, -0.5, 0,
-    0.5, 0.5, 0,
+    0.5, -0.5, 0,
     0, 0.5, 0
   ]
+
+compileShader :: FilePath -> GLenum -> IO GLuint
+compileShader shaderFileName shaderType = do
+  shaderSrc <- BS.readFile =<< getDataFileName ("shaders/" ++ shaderFileName)
+  shaderId <- glCreateShader shaderType
+  BS.unsafeUseAsCString shaderSrc $ \shaderSrcPtr ->
+    FMU.with shaderSrcPtr $ \shaderSrcPtrPtr ->
+      glShaderSource shaderId 1 shaderSrcPtrPtr nullPtr
+  glCompileShader shaderId
+  compilationFailed <- alloca $ \compileStatusPtr -> do
+    glGetShaderiv shaderId GL_COMPILE_STATUS compileStatusPtr
+    (GL_FALSE ==) . fromIntegral <$> peek compileStatusPtr
+  when compilationFailed $ do
+    let maxLen = 512
+    infoLogText <- fmap T.decodeUtf8Lenient . BS.createUptoN maxLen $ \infoLogBsPtr ->
+      alloca $ \actualLenPtr -> do
+        glGetShaderInfoLog shaderId (fromIntegral maxLen) actualLenPtr (castPtr infoLogBsPtr)
+        fromIntegral <$> peek actualLenPtr
+    throwAppExM $ "Compile error for shader '" ++ shaderFileName ++ "': " ++ T.unpack infoLogText
+  return shaderId
